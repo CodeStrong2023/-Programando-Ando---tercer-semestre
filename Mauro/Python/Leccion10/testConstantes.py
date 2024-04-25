8ff91a1515210f002788b0a7753164995926605d
Mauro <mauromesas98@gmail.com>
2024-04-25 20:59:35 -0300

    add python

---
+++ Mauro/Python/Leccion10/testConstantes.py
@@ -0,0 +1,8 @@
+from Constantes import *
+from Constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+#MI_CONSTANTE = 'Nuevo valor de constante'
+#print(MI_CONSTANTE) Esto no se debe hacer
+
+print(Mate)
