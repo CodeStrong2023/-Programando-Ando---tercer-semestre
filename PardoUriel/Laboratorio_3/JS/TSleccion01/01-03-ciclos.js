a8f5414c2bfa240efaf585ac4e06ecdec738b7de
unknown <urielpardod712@gmail.com>
2024-05-16 15:09:40 -0300

    Leccion 01 JS

---
+++ PardoUriel/Laboratorio_3/JS/TSleccion01/01-03-ciclos.js
@@ -0,0 +1,50 @@
+let contador = 0;
+while(contador <3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while")
+
+// Do While
+
+//Primero se va a ejecutar el codigo y luego revisa la condicion
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo < 3);
+console.log("fin del ciclo");
+
+// For
+for( let contando = 0; contando < 3; contando++){
+    console.log(contando);
+}
+console.log("fin del ciclo");
+
+// Palabra reservada Break
+for( let contando = 0; contando <= 10; contando++){
+    if(contando % 2 == 0){
+        console.log(contando);
+        break //quiebra el ciclo y sale
+    }
+}
+console.log("Fin del ciclo de pares");
+
+// Palabra Continue
+for( let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        continue; // ir a la siguente iteracion
+    }
+    console.log(contando);
+}
+console.log("fin del ciclo");
+
+// Etiquetas Labels
+inicio: //indicamos que rompa el ciclo for
+for( let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; // quiebra el ciclo y hace un goingto a la etiqueta
+    }
+    console.log(contando);
+}
+console.log("fin del ciclo");
