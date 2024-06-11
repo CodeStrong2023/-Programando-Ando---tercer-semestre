c7f9fb0a69d6e3cbeacbe6b385b01f033b7ae562
Alitoo27 <perezalexismartin575@gmail.com>
2024-06-11 20:05:09 -0300

    python clase 9

--- Alexis/Python/Leccion04/BD/prueba_bd.py
+++ Alexis/Python/Leccion04/BD/prueba_bd.py
@@ -1,21 +1,24 @@
 import psycopg2  # esto es para poder conectanos a potgre
 
 conexion = psycopg2.connect(
-    user ='postgres',
+    user='postgres',
     password='admin',
     host='127.0.0.1',
     port='5432',
     database='test_bd'
 )
-
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-conexion.execute(sentencia)  # de esta manera ejecutamos la sentencia
-registros = cursos.fetchall()  # recuperamos todos los registros que seran una lista
-print(registros)
-
-cursor.close()
-conexion.close()
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'  # Placeholder
+            id_persona = input('digite un numero para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,))  # de esta manera ejecutamos la sentencia
+            registros = cursos.fetchone()  # recuperamos todos los registros que seran una lista
+            print(registros)
+except Exception as e:
+    print(f'ocurrio un error: {e}')
+finally:
+    conexion.close()
 
 
 
